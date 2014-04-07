57a5e770309fe6a17cd136020628f82374a2918c
Andrey Repin <aarepin@gmail.com>
2014-04-07 20:19:52 +0300

    incomplete

--- temperature.rb
+++ temperature.rb
@@ -1,8 +1,12 @@
 class Temperature
 
 def initialize(a)
-	@m = Array.new(a){|el| el = rand((Time.now - 60*60*24*365)..Time.now).strftime('%d.%m.%y') + ' ' + rand(-30..30).to_s }		
+	@m = Array.new(a){|el| el = rand((Time.now - 60*60*24*365)..Time.now).strftime('%d.%m') + ' ' + rand(-30..30).to_s }		
 end	
+
+def average_temp
+	
+end
 
 z = Temperature.new(40)
 p z
