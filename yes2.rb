fd59cdf31090c702cc0bd39e88b35f1bf8e7bfa8
Andrey Repin <aarepin@gmail.com>
2014-04-09 09:29:19 +0300

    not done

--- yes2.rb
+++ yes2.rb
@@ -1,1 +1,3 @@
 h = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', :yesss => :fg, try: {'yesterday1' => 34, 'yesteryear' => 2014}, key: [{'yes' => 23, 'b' => 'travel'}], 'yesterday1' => 34, 'yesteryear' => 2014}
+
+puts h
